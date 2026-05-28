d22d1a9e059b3e0e712b986775ad53dde17f859b
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2026-05-28 23:38:02 +0530

    C3 feat(client): implement search box toggle in NavbarComponent

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -1,9 +1,13 @@
 import {Link} from 'react-router-dom'
 import logo from '../imgs/logo.png'
+import { useState } from 'react'
 
 
 
 const NavbarComponent = () => {
+
+  const [searchBoxVisibility, setSearchBoxVisibility] = useState(false)
+
   return (
     <nav className='navbar'>
 
@@ -11,7 +15,7 @@ const NavbarComponent = () => {
         <img src={logo} className='w-full' />
       </Link>
 
-      <div className='absolute bg-white w-full left-0 top-full mt-0.5 border-b border-gray-300 py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto'>
+      <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-gray-300 py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto " + (searchBoxVisibility ? "block" : "hidden" )}>
         <input 
           type="text"
           placeholder="Search"
@@ -19,6 +23,14 @@ const NavbarComponent = () => {
         />
 
         <i class="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-gray-600"></i>
+      </div>
+
+      <div className='flex items-center gap-3 md:gap-6 ml-auto'>
+        <button className='md:hidden bg-gray-200 w-12 h-12 rounded-full flex items-center justify-center'
+          onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}
+        >
+          <i className='fi fi-rr-search text-xl text-gray-600'></i>
+        </button>
       </div>
         
       
