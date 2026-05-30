5a45897de7c60b9b2e07a37a9f89d07ef4c1328a
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2026-05-30 15:16:05 +0530

    C14 fix(client): correct button tag in UserAuthFormPage and update className in NavbarComponent

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -23,7 +23,7 @@ const NavbarComponent = () => {
             className='w-full md:w-auto bg-gray-100 p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-gray md:pl-12'
           />
 
-          <i class="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-gray-600"></i>
+          <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-gray-600"></i>
         </div>
 
         <div className='flex items-center gap-3 md:gap-6 ml-auto'>
--- client/src/pages/UserAuthFormPage.jsx
+++ client/src/pages/UserAuthFormPage.jsx
@@ -38,12 +38,12 @@ const UserAuthFormPage = ({ type }) => {
                     icon="fi-rr-key" 
                 />
 
-                <buton
-                    className="btn-dark center mt-14 w-fit"
+                <button
+                    className="btn-dark center mt-14"
                     type="submit"
                 >
                     {type == "signin" ? "Sign In" : "Sign Up"}
-                </buton>      
+                </button>      
 
                 <div className='relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold'>
                     <hr className='w-1/2 border-back'/>
