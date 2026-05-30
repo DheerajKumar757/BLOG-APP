a1c3ac10d0e0bd2c803a032b3ef93a3021d1474c
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2026-05-30 09:33:51 +0530

    C7 feat(client): add InputComponent and integrate it into UserAuthFormPage for form handling

--- client/src/pages/UserAuthFormPage.jsx
+++ client/src/pages/UserAuthFormPage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import InputComponent from '../components/InputComponent'
 
 const UserAuthFormPage = ({ type }) => {
   return (
-    <h1>{type}</h1>
+    <section className='h-cover flex items-center justify-center'>
+        <form className='w-[80%] max-w-[400px]'>
+            <h1 className='text-4xl font-gelasio capitalize text-center mb-24'>
+                {type == "signin" ? "Welcome Back!" : "Join us today"}
+            </h1>
+
+            {
+                type != "signin" ?
+                    <InputComponent name="fullname" type="text" placeholder="full name" />
+                : ""
+            }
+        </form>
+
+    </section>
   )
 }
 
